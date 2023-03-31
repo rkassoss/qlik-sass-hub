f481f923c1222976d866df846e1b7e920ff49b1d
Ran Kassovitz <kassoran@gmail.com>
2023-03-31 15:01:54 -0400

    connect to qlik desktop and list app

---
+++ src/components/AppList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class AppList extends React.Component {
+
+    constructor () {
+        super()
+        this.state = { apps: [] }
+    }
+
+    componentDidMount(){
+        this.getApps(this.props.engine)
+    }
+    
+    //Get list of apps
+    getApps = (engine) => {
+        engine.then(engine => {
+            engine.getDocList().then(apps => {
+                //update state with list of apps from object
+                this.setState({ apps: apps })
+            })
+        });
+    }
+
+  render() {
+    //create a variable containing app state
+    const appList = this.state.apps
+
+    return (
+      <div>
+        <h1>AppList</h1>
+        {/* Map over app object and render our app list */}
+        <div> {appList.map((appList,i) => <div key={i}> {appList.qDocName} </div>)} </div>
+      </div>
+    );
+  }
+}
+
+export default AppList;
---
+++ src/engine/engine.js
@@ -0,0 +1,13 @@
+import enigma from 'enigma.js'
+
+const schema = require("enigma.js/schemas/3.1.json")
+
+//create a new session
+const session = enigma.create({
+    schema,
+    url: 'ws://localhost:4848/app/engineData'
+})
+
+export default session.open().then(global => {
+   return global
+});
